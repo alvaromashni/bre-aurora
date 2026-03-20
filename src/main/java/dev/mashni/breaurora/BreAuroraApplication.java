5615d81360692683c71d55269d15f27796f429ec
Álvaro Mashni <alvinhoremor.sm@gmail.com>
2026-03-20 14:52:35 -0300

    config inicial

---
+++ src/main/java/dev/mashni/breaurora/BreAuroraApplication.java
@@ -0,0 +1,13 @@
+package dev.mashni.breaurora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreAuroraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreAuroraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mashni/breaurora/BreAuroraApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mashni.breaurora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreAuroraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
